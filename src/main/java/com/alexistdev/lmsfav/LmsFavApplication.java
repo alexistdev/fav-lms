bdf6a65f89644d7abe76ef6f84c9fb974ed3bbfb
Alexsander Hendra Wijaya <alexistdev@gmail.com>
2023-10-23 13:21:47 +0700

    feat(first-generate): LMS Version 1

---
+++ src/main/java/com/alexistdev/lmsfav/LmsFavApplication.java
@@ -0,0 +1,13 @@
+package com.alexistdev.lmsfav;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmsFavApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LmsFavApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alexistdev/lmsfav/LmsFavApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexistdev.lmsfav;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmsFavApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
